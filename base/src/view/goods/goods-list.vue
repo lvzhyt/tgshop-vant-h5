564692e64cce0aaf798f91cdd20a87e4d7f0cc03
lvzhyt <lvzhyt@163.com>
2019-07-15 10:29:34 +0800

    goodsList

--- base/src/view/goods/goods-list.vue
+++ base/src/view/goods/goods-list.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="goods-list-container">
         <van-row>
             <van-col span="1">
                 <div class="search-nav" @click="onNavBackClick">
@@ -14,10 +14,49 @@
                         shape="round"
                         @focus="onSearchFocus"
                 >
-<!--                    <div slot="action" @click="onSearchClick">搜索</div>-->
+                    <div slot="action" @click="onSearchFocus">搜索</div>
                 </van-search>
             </van-col>
         </van-row>
+        <van-list
+                v-model="loading"
+                :finished="finished"
+                finished-text="没有更多了"
+                @load="onLoad"
+        >
+
+            <van-card
+                    v-for="item in list"
+                    class="goods-card"
+                    desc="描述信息"
+                    title="商品标题"
+                    thumb="https://img.yzcdn.cn/vant/t-thirt.jpg"
+                    @click="onGoodsItemClick(item)"
+            >
+                <div slot="tags" class="goods-item-price">
+                    <span class="price-pre">￥</span>
+                    <span class="price-val">99</span>
+                    <span class="price-origin">￥199</span>
+                </div>
+                <div slot="price" >
+                    <van-tag  type="danger" class="goods-item-tag">标签</van-tag>
+                    <van-tag plain type="danger"class="goods-item-tag">标签</van-tag>
+                </div>
+                <div slot="bottom">
+                    <div class="goods-item-bottom">评价15672</div>
+                    <div class="goods-item-bottom">店铺</div>
+                    <van-divider class = "goods-item-divider"/>
+                </div>
+<!--                <div slot="footer">-->
+<!--                    <van-button size="mini">按钮</van-button>-->
+<!--                    <van-button size="mini">按钮</van-button>-->
+<!--                </div>-->
+
+            </van-card>
+
+        </van-list>
+
+
         <div>
             goodsList
         </div>
@@ -27,6 +66,7 @@
 <script>
     import { NavBar,Icon,Search } from 'vant';
     import { Row, Col } from 'vant';
+    import { Card,Tag,Button,List,Cell,Divider   } from 'vant';
 
     export default {
         name: "goods-list",
@@ -35,12 +75,21 @@
             [Search.name]:Search,
             [Row.name]:Row,
             [Col.name]:Col,
+            [Card.name]:Card,
+            [Tag.name]:Tag,
+            [Button.name]:Button,
+            [List .name]:List ,
+            [Cell .name]:Cell ,
+            [Divider .name]:Divider ,
             [Icon.name]:Icon
         },
         data() {
             return {
                 searchValue: '',
-                searchPlaceHolder: '请输入搜索关键词'
+                searchPlaceHolder: '请输入搜索关键词',
+                list: [],
+                loading: false,
+                finished: false
             }
         },
         methods:{
@@ -67,6 +116,33 @@
                     }
                 }
                 this.$router.push(option)
+            },
+            onLoad() {
+                // 异步更新数据
+                setTimeout(() => {
+                    for (let i = 0; i < 10; i++) {
+                        let item = {
+
+                        }
+                        this.list.push(this.list.length + 1);
+                    }
+                    // 加载状态结束
+                    this.loading = false;
+
+                    // 数据全部加载完成
+                    if (this.list.length >= 40) {
+                        this.finished = true;
+                    }
+                }, 500);
+            },
+            onGoodsItemClick(item){
+                let option = {
+                    name: 'goods',
+                    params: {
+                        skuId: item
+                    }
+                }
+                this.$router.push(option)
             }
         },
         mounted() {
@@ -81,5 +157,52 @@
 </script>
 
 <style scoped>
+    .goods-list-container{
+        background-color: #ffffff;
+    }
+    .search-nav{
+        background-color: #ffffff;
+        height:53px;
+        width: 31px;
+        vertical-align: middle;
+        text-align: center;
+    }
+    .search-nav i{
+        margin-top: 18px;
+    }
+    .goods-card{
+        background-color: #ffffff;
+    }
+    .goods-item-divider{
 
+    }
+    .goods-item-tag{
+        margin-right: 5px;
+        padding: 2px;
+    }
+    .goods-item-price{
+        padding-top: 10px;
+        color: #ff4444;
+
+    }
+    .price-pre{
+        font-size: 13px;
+
+    }
+    .price-val{
+        font-size: 16px;
+        font-weight:bold;
+        /*color: #ff4444;*/
+        /*font-weight:bold;*/
+    }
+    .price-origin{
+        font-size: 12px;
+        color: #979797;
+        margin-left: 5px;
+        text-decoration:line-through
+    }
+    .goods-item-bottom{
+        font-size: 10px;
+        color: #898989;
+    }
 </style>
