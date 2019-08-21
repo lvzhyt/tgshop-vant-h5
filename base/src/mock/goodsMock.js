4589f7d63a3aeffc78e0ea390bab026f990899b4
lvzhyt <lvzhyt@163.com>
2019-08-21 11:40:37 +0800

    goods detail

--- base/src/mock/goodsMock.js
+++ base/src/mock/goodsMock.js
@@ -1,6 +1,6 @@
 import {chooseSkuSpecData} from "../data/chooseSkuSpecData";
 
-
+/* eslint-disable */
 export const getChooseSku = req => {
     console.log('mock '+req.url)
     return chooseSkuSpecData
